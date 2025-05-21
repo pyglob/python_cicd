3c0ccbe8fb3f6dbd6fe8e31ead71451b9133f85c
pyglob <pyglob@gmail.com>
2025-05-21 08:59:07 +0300

    Create testpython.py
    simple python script for testing

---
+++ testpython.py
@@ -0,0 +1,12 @@
+my_list = ['a', 'b', 'a', 'c', 'b','a']
+print(my_list)
+#new_list = list(set(my_list))
+
+newlist = []
+
+# for item in my_list:
+#     if item not in newlist:
+#         newlist.append(item)
+
+
+print(newlist)
